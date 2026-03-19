013ee85bd4ec2294decef251871aef644ada062c
Dylan <dylan@bakerfive.com>
2026-03-19 11:33:55 +1000

    Wire up Redux Provider and RouterProvider in App.tsx

---
+++ src/router/index.tsx
@@ -0,0 +1,21 @@
+import DashBoard from "@/pages/Dashboard";
+import LoginPage from "@/pages/LoginPage";
+import NotFound from "@/pages/NotFound";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginPage />
+  },
+  {
+    path: '/',
+    element: <DashBoard />
+  },
+  {
+    path: '/',
+    element: <NotFound />
+  }
+])
+
+export default router
