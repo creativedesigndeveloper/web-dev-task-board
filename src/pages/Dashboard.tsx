80087c40908bcd29d4accff5dcac9e7e8a206070
Dylan <dylan@bakerfive.com>
2026-03-19 13:41:46 +1000

    Build dashboard with user info and logout functionality

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,25 @@
+import { useAppDispatch, useAppSelector } from "@/hooks/useAppDispatch"
+import { logout } from "@/store/authSlice"
+import { useNavigate } from "react-router-dom"
+
 const DashBoard = () => {
-  return <div>Dashboard</div>
+  const userName = useAppSelector((state) => state.auth.user?.name)
+  const email = useAppSelector((state) => state.auth.user?.email)
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const userLogout = () => {
+    console.log(userName)
+    dispatch(logout())
+    navigate('/')
+  }
+
+
+  return (
+    <>
+      <h1>{`Hello ${userName} with email ${email} `}</h1>
+      <button onClick={userLogout}>Logout</button>
+    </>
+  )
 }
 
 export default DashBoard
