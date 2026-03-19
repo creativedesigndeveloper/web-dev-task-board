49c5754684bccc7e21149a016ed0cff1d6b4a425
Dylan <dylan@bakerfive.com>
2026-03-19 12:15:46 +1000

    Add login form with controlled inputs and form submission

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react"
+
 const LoginPage = () => {
-  return <div>LoginPage</div>
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log(email, password)
+    setEmail('')
+    setPassword('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h4>Email</h4>
+          <input value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Enter Email" />
+        </div>
+        <div>
+          <h4>Password</h4>
+          <input value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Enter password" />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+
+      </form>
+    </div>
+  )
 }
 
 export default LoginPage
