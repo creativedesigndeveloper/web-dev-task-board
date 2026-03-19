e9687f6089bfb63e37943707fb2ba0b728ca7c21
Dylan <dylan@bakerfive.com>
2026-03-19 10:54:00 +1000

    Add Redux store and auth slice

---
+++ src/store/authSlice.ts
@@ -0,0 +1,30 @@
+import type { AuthState, User } from "@/types/auth";
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+const initialState: AuthState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+}
+
+export const authSlice = createSlice({
+  name: 'authSlice',
+  initialState,
+  reducers: {
+    setCredentials: (state, action: PayloadAction<{user: User, token: string}>) => {
+      state.user = action.payload.user
+      state.token = action.payload.token
+    },
+    logout: (state) => {
+      state.user = null
+      state.token = null
+      state.isAuthenticated = false
+    },
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload
+  }}
+})
+
+export const {setCredentials, logout, setUser} = authSlice.actions
+export default authSlice.reducer
